418b6fbd81281d87e159a41f8c6e3f06f513744c
unknown <alicebe.phan@gmail.com>
2022-01-18 02:43:56 -0600

    Updated Home component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h1>THU PHAN</h1>
+            <h3>A Full Stack Web Developer</h3>
+            <p>If you don't try, you will never know.</p>
+            
+        </div>
+    )
+}
+
+export default Home
