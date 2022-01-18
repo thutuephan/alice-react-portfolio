9fc6b0cb6bbdbc125ff524d7282fc5f8e328de24
unknown <alicebe.phan@gmail.com>
2022-01-18 02:10:55 -0600

    Created and updated Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer className='footer'>
+                <div className='icons'>
+                    <ul className='list-style: none list-inline text-center'>
+                        <li className='list-inline-item'>
+                            <a target="_blank" href="https://github.com/thutuephan"><i className="fab fa-github-square fa-2x"></i>
+                            </a>
+                        </li>
+                        <li className='list-inline-item'>
+                            <a target="_blank" href="https://www.linkedin.com/in/thu-phan-2abaa1221/"><i className="fab fa-linkedin-in fa-2x"></i>
+                            </a>
+                        </li>
+
+                    </ul>
+
+                </div>
+                <p> Created by Thu Phan</p>
+
+            </footer>
+            
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigation({ currentPage, handlePageChange }) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navigation
