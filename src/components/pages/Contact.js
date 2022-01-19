9026f1beab3b7a36882481cfe9d93b605d655382
unknown <alicebe.phan@gmail.com>
2022-01-19 10:38:30 -0600

    Got an error saying cannot resolve component.js

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+            <h1>Works</h1>
+            <p>
+            While I wrote the initial version of this extension, many people (too many to name) have helped make it what it is today. From providing TypeScript definitions to keeping up with changing API and best practices. If you are enjoying this extension, you have the great React community to thank.
+
+
+            </p>
+        </div>
+    )
+}
+
+export default Contact;
