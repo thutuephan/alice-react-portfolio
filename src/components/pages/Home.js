4c9d812f7f105b72f194985f1850330a651de700
unknown <alicebe.phan@gmail.com>
2022-01-18 22:33:28 -0600

    Added styles to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
 
+import './styles/footer.css'
+
+const styles = {
+    homeStyle: {
+        background: '#050A30',
+        
+    },
+    homeText: {
+        fontFamily: 'Luxurious Roman', cursive,
+        fontWeight: 'bolder',
+        color: 'white',
+        justifyContent: 'center',
+
+    }
+}
 function Home() {
     return (
         <div>
-            <h1>THU PHAN</h1>
-            <h3>A Full Stack Web Developer</h3>
-            <p>If you don't try, you will never know.</p>
+            <div className="home container" style={styles.homeStyle}>
+                <h1 className="home-text" stsyle={styles.homeText}>THU PHAN</h1>
+                <h3 className="title" style={styles.homeText}>A Full Stack Web Developer</h3>
+                <p className="quote" style={styles.homeText}>If you don't try, you will never know.</p>
+            </div>
+            
             
         </div>
     )
